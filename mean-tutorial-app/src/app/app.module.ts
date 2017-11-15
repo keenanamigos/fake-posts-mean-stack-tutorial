55402159f500d6ee6e9c53818a0e5b13599045cb
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-15 18:50:27 -0500

    Use ng generate component to add posts component and import the RouterModule in app.module.ts

--- mean-tutorial-app/src/app/app.module.ts
+++ mean-tutorial-app/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
