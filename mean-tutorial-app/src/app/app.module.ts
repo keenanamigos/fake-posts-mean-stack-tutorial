14c63f70a1ac3a749dc4fb046e5189f170c64d74
Keenan Johnson <keenan.johnson1@outlook.com>
2017-11-15 19:12:23 -0500

    Import PostsService (after generating) in the app.module.ts file Add PostsService to the providers array
    Import Http and map in the posts.service.ts file
    Set up constructor
    Create a getAllPosts function
    
    Import PostsService to the posts.component.ts file
    Set up constructor to take an instance of the postsService
    Update ngOnInit function to call getAllPosts and update initialized posts array to the value of the response from the API

--- mean-tutorial-app/src/app/app.module.ts
+++ mean-tutorial-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { PostsService } from './posts.service';
 
 // Define the routes
 const ROUTES = [
@@ -31,7 +32,7 @@ const ROUTES = [
     HttpModule,
     RouterModule.forRoot(ROUTES) // Add routes to the app
   ],
-  providers: [],
+  providers: [PostsService], // Add service to the app
   bootstrap: [AppComponent]
 })
 export class AppModule { }
